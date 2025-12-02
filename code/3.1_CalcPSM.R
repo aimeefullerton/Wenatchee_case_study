3d170d9fb728d6d898d69eed9c617bd503289153
Aimee.Fullerton <1365855290113302@mil>
2025-12-02 14:47:31 -0800

    Reformat Figure 7 as annual values in ggplot

--- code/3.1_CalcPSM.R
+++ code/3.1_CalcPSM.R
@@ -85,22 +85,52 @@ NewData <- with(NewData,
 write.csv(NewData, paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
 
 
-# Visualize results with CIs of PSM
 
-plot1 <- NewData |> subset(Origin == 0) |>
-  ggplot(aes(x = ST.metric, y = psmhi, color = year))+
+# Figure 7 ---- 
+st.mn <- d2p %>% 
+  group_by(year) %>%
+  reframe("ST.metric" = mean(ST.metric, na.rm = T))
+psm <- cbind(st.mn, psm.mn[,2], psm.min[,2], psm.med[,2], psm.max[,2])
+colnames(psm) <- c("year", "ST", "Mean", "Min", "Med", "Max")
+
+plot1 <- psm |> ggplot() +
+  geom_point(aes(x = ST, y = Mean, color = year), size = 3, alpha = 0.5)+
+  geom_point(aes(x = ST, y = Max, color = year), size = 3, alpha = 0.5, shape = 17)+
+  geom_point(aes(x = ST, y = Min, color = year), size = 3, alpha = 0.5, shape = 17)+
+  geom_smooth(aes(x = ST, y = Mean), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = ST, y = Max), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = ST, y = Min), method = "loess", se = TRUE, color = "gray50") +
   scale_color_gradient(low = "blue", high = "red")+
-  geom_point(size = 1, alpha = 0.3)+
-  theme(axis.text = element_text(size=12))+
-  theme(axis.title = element_text(size=12))+
+  theme(axis.text = element_text(size = 12))+
+  theme(axis.title = element_text(size = 12))+
   theme(axis.title.y = element_text(vjust = 3.5))+
   theme(axis.title.x = element_text(vjust = -1))+
   labs(x = "Mean August stream temperature (°C)")+
   labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
-  scale_x_continuous(limits = c(0, 25))+
+  scale_x_continuous(limits = c(10, 20))+
   scale_y_continuous(limits = c(0, 1))
 
-ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM_all.png"), plot1, height = 4.25, width = 6)
+ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM.png"), plot1, height = 4.25, width = 6)
+
+
+plot1 <- psm |> ggplot() +
+  geom_point(aes(x = year, y = Mean, color = ST), size = 2, alpha = 0.5)+
+  geom_point(aes(x = year, y = Max, color = ST), size = 2, alpha = 0.5, shape = 17)+
+  geom_point(aes(x = year, y = Min, color = ST), size = 2, alpha = 0.5, shape = 15)+
+  geom_smooth(aes(x = year, y = Mean), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = year, y = Max), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = year, y = Min), method = "loess", se = TRUE, color = "gray50") +
+  scale_color_gradient(low = "blue", high = "red")+
+  theme(axis.text = element_text(size = 12))+
+  theme(axis.title = element_text(size = 12))+
+  theme(axis.title.y = element_text(vjust = 3.5))+
+  theme(axis.title.x = element_text(vjust = -1))+
+  labs(x = "Year")+
+  labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
+  #scale_x_continuous(limits = c(10, 20))+
+  scale_y_continuous(limits = c(0, 1))
+
+ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM_2.png"), plot1, height = 4.25, width = 6)
 
 
 # by Origin
@@ -108,47 +138,41 @@ ggplot(NewData, aes(x = ST.metric, y = PSMPred, color = rev(as.factor(Origin)), 
   geom_point()+
   theme(legend.position = "none")
 
-
-# Plot over time ----
+# Alternative format Figure 7A ----
 NewData <- data.table::fread(paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
 d2p <- NewData[NewData$Origin == 0, c("year", "COMID", "ST.metric", "PSMPred", "psmlo", "psmhi")]
 d2p <- d2p[order(d2p$year, d2p$COMID),]
-  #d2p <- d2p[d2p$COMID %in% cids,]
+#d2p <- d2p[d2p$COMID %in% cids,]
 
 yrs <- sort(unique(d2p$year))
-psm_minmax <- data.frame("year" = sort(rep(seq(min(yrs), max(yrs)), 3)))
-for(var in c("PSMPred", "psmlo", "psmhi", "ST.metric")){
-  var_sym <- sym(var)
-  result <- d2p %>% 
-    group_by(year) %>%
-    reframe(quantile(!!var_sym, c(0.05, 0.5, 0.95), na.rm = T), prob = c(0.05, 0.5, 0.95))
-  colnames(result)[ncol(result) -1] <- var
-  if(var == "ST.metric"){
-    psm_minmax <- cbind(psm_minmax, result[, 2:3])
-  } else {
-    psm_minmax <- cbind(psm_minmax, result[, 2])
-    
-  }
-}
-colnames(psm_minmax) <- c("year", "PSMPred", "psmlo", "psmhi", "ST.metric", "quantile")
+result <- d2p %>% 
+  group_by(year) %>%
+  reframe(quantile(psmhi, c(0.05, 0.5, 0.95), na.rm = T), prob = c(0.05, 0.5, 0.95))
+colnames(result) <- c("year", "psmhi", "quantile")
 
-psm.min <- psm_minmax[psm_minmax$quantile == 0.05,]
-psm.max <- psm_minmax[psm_minmax$quantile == 0.95,]
-psm.med <- psm_minmax[psm_minmax$quantile == 0.5,]
+psm.min <- result[result$quantile == 0.05,-ncol(result)]
+psm.max <- result[result$quantile == 0.95,-ncol(result)]
+psm.med <- result[result$quantile == 0.5,-ncol(result)]
+psm.mn <- d2p %>% 
+  group_by(year) %>%
+  reframe("psmhi" = mean(psmhi, na.rm = T))
 
-f1 <- predict(loess(psm.max$PSMPred ~ psm.max$year, span = 0.5))
-f2 <- predict(loess(psm.max$psmhi ~ psm.max$year, span = 0.5))
-f3 <- predict(loess(psm.max$psmlo ~ psm.max$year, span = 0.5))
+f1 <- predict(loess(psm.max$psmhi ~ psm.max$year, span = 0.5))
+f2 <- predict(loess(psm.mn$psmhi ~ psm.mn$year, span = 0.5))
+f3 <- predict(loess(psm.min$psmhi ~ psm.min$year, span = 0.5))
 
-png("plots/PSM_over_time_spwn_subset.png", width = 6, height = 6, units = "in", res = 300)
+png("plots/PSM_over_time.png", width = 6, height = 6, units = "in", res = 300)
 
-  plot(psm.max$year, psm.max$PSMPred, cex = .3, type = "n",
-      las = 1, ylab = "Predicted pre-spawn mortality for wild Chinook salmon", xlab = "Year", ylim = c(0,1))
-  polygon(c(psm.min$year, rev(psm.max$year)), c(psm.min$psmlo, rev(f3)), col = "#0C7BDC80", border = NA)
-  polygon(c(psm.min$year, rev(psm.max$year)), c(psm.min$psmhi, rev(f2)), col = "#0C7BDC80", border = NA)
-  polygon(c(psm.min$year, rev(psm.max$year)), c(psm.min$PSMPred, rev(f1)), col = "#0C7BDC80", border = NA)
-  points(psm.max$year, psm.max$psmlo, pch = 25, cex = 0.5, bg = "#0965B5", col = "#0965B5")
-  points(psm.max$year, psm.max$PSMPred, pch = 21, , cex = 0.7, bg = "#0C7BDC", col = "#0C7BDC")
-  points(psm.max$year, psm.max$psmhi, pch = 24, cex = 0.5, bg = "#A9C9E8", col = "#0C7BDC")
+plot(psm.max$year, psm.max$psmhi, cex = .3, type = "n",
+     las = 1, ylab = "Predicted pre-spawn mortality for wild Chinook salmon", xlab = "Year", ylim = c(0,1))
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f3)), col = "#0C7BDC80", border = NA)
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f2)), col = "#0C7BDC80", border = NA)
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f1)), col = "#0C7BDC80", border = NA)
+lines(psm.max$year, f3, lwd = 2, col = "#0965B5")
+lines(psm.max$year, f2, lwd = 2, col = "#0C7BDC")
+lines(psm.max$year, f1, lwd = 2, col = "#A9C9E8")
+points(psm.max$year, psm.min$psmhi, pch = 25, cex = 0.5, bg = "#0965B5", col = "#0965B5")
+points(psm.max$year, psm.mn$psmhi, pch = 21, , cex = 0.5, bg = "#0C7BDC", col = "#0C7BDC")
+points(psm.max$year, psm.max$psmhi, pch = 24, cex = 0.5, bg = "#A9C9E8", col = "#0C7BDC")
 
 dev.off()
