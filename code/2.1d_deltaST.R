d923771ad831f958a3b730c61e136c31f0587d9e
Aimee.Fullerton <1365855290113302@mil>
2026-01-02 14:28:28 -0800

    add updated steps

---
+++ code/2.1d_deltaST.R
@@ -0,0 +1,295 @@
+# Calculate a delta stream temperature for future periods
+library(dplyr)
+library(data.table)
+library(ggplot2)
+library(sf)
+
+# Aggregate function
+fncAggBy <- function(dat, filter_month = 1:12, reach = COMID, timestep = doy, stat = "mean", p = 0.95){
+    result <- dat |>
+      mutate(
+        month = as.character(lubridate::month(Date)),
+        year = as.character(lubridate::year(Date)),
+        doy = as.character(lubridate::yday(Date))) |>
+      group_by({{reach}}, {{timestep}}) |>
+      filter(month %in% filter_month)
+  if(stat == "mean"){
+      result <- result |>
+        summarise(across(
+          where(is.numeric),
+          ~ mean(.x, na.rm = T),
+          .names = "mean_{.col}"),
+          .groups = "drop") 
+  } else { # quantile
+      result <- result |>
+        summarise(across(
+          where(is.numeric),
+          ~ quantile(.x, probs = p, na.rm = T),
+          .names = paste0("q", substr(p,3,4), "_{.col}")),
+          .groups = "drop")
+  }
+  return(result)
+}
+ 
+
+# Prepare and aggregate empirical data ----
+emp <- data.table::fread("data/WenTemps3Jun25.csv")
+emp <- emp[!is.na(emp$COMID), c("Date", "COMID", "AvgDailyTemp")]
+
+# Aggregate by day of year and reach
+emp_agg <- cbind(fncAggBy(dat = emp, stat = "q", p = 0.05), fncAggBy(dat = emp, stat = "mean")[,3], 
+                 fncAggBy(dat = emp, stat = "q", p = 0.95)[,3])
+emp_agg$doy <- as.numeric(emp_agg$doy); emp_agg <- emp_agg[order(emp_agg$COMID, emp_agg$doy),]
+data.table::fwrite(emp_agg, "data/emp_agg.csv")
+
+
+# Prepare and aggregate retrospective period (1990-2020) predictions from the localized model ----
+st <- data.table::fread("data/st_pred_Wenatchee.csv")
+st <- st[!is.na(st$prd.stream_temp),]
+st <- st[,c("COMID", "tim.date", "prd.stream_temp")]
+colnames(st)[2] <- "Date"
+
+  # evaluate and remove unrealistically high predictions
+  nrow(st[st$prd.stream_temp >= 30,]) / nrow(st) * 100
+  hist(st$prd.stream_temp)
+  idx <- which(st$prd.stream_temp >=30)
+  foo <- st[idx,]
+  foo$doy <- lubridate::yday(foo$Date)
+  plot(foo$doy, foo$prd.stream_temp)
+  hist(lubridate::year(foo$Date))
+  cids <- sort(unique(foo$COMID))
+  streams <- read_sf("data/shp/upper_columbia_streams_nhd2.shp")
+  spawn_reaches <- read.csv("data/Wenatchee_spawn_reaches.csv")
+  streams <- dplyr::left_join(streams, spawn_reaches, join_by(COMID))
+  ss <- streams[streams$COMID %in% cids,] #none are spawning reaches - many are unnamed
+  # likely safe to remove these anomalous reaches
+  st <- st[-idx,]
+  rm(cids, idx, foo, ss)
+
+# Aggregate retrospective period localized model predictions by day of year and reach
+retro_agg <- cbind(fncAggBy(dat = st, stat = "q", p = 0.05), fncAggBy(dat = st, stat = "mean")[,3], 
+                 fncAggBy(dat = st, stat = "q", p = 0.95)[,3])
+retro_agg$doy <- as.numeric(retro_agg$doy); retro_agg <- retro_agg[order(retro_agg$COMID, retro_agg$doy),]
+data.table::fwrite(retro_agg, "data/retro_agg.csv")
+
+
+# Prepare and aggregate future period predictions ----
+setwd("D://st-cc/nc_preds")
+fls <- dir(getwd())
+fl <- fls[grep(".csv", fls)]
+basin <- 17020011
+fl <- fl[grep(basin, fl)]
+climate_scenarios <- c("CanESM2", "CCSM4", "CNRM-CM5", "CSIRO-Mk3-6-0", "GDFL-ESM2M", "HadGEM2-CC", "HadGEM2-ES", "inmcm4", "IPSL-CM5A-MR", "MIROC")
+climate_scenarios <- gsub("-", "_", climate_scenarios)
+years <- 1990:2099
+
+# Collate single data table
+hucdata <- NULL
+for(huc10 in 1:length(fl)){
+  huc10_data <- data.table::fread(fl[huc10])
+  cids <- sort(unique(huc10_data$COMID))
+  cols <- c("date", "COMID", climate_scenarios)
+  f <- huc10_data[,..cols]
+  f <- subset(f, lubridate::year(f$date) %in% years) # exclude older data we have no need for
+  hucdata <- rbind(hucdata, f)
+}
+colnames(hucdata)[1] <- "Date"
+
+# Remove unrealistically high predictions (and NA values)
+filtered <- hucdata %>%
+  filter(if_all(3:12, ~ .x < 30))
+setwd("~/GitHub/Wenatchee_case_study")
+data.table::fwrite(filtered, "data/future_wide.csv")
+
+# Pivot longer (COMID, date, GCM, STvalue)
+fut_long <- tidyr::pivot_longer(data = filtered, 
+                    cols = 'CanESM2':'MIROC',
+                    names_to = "GCM",
+                    values_to = "STvalue")
+data.table::fwrite(fut_long, "data/future_long.csv")
+
+
+# Aggregate ensemble GCM estimates by day of year and reach for 1990-2020 period
+period_data <- fut_long[lubridate::year(fut_long$Date) > 1989 & lubridate::year(fut_long$Date) < 2021, c("Date", "COMID", "STvalue")]
+agg9020 <- cbind(fncAggBy(dat = period_data, stat = "q", p = 0.05), fncAggBy(dat = period_data, stat = "mean")[,3], 
+                   fncAggBy(dat = period_data, stat = "q", p = 0.95)[,3])
+agg9020$doy <- as.numeric(agg9020$doy); agg9020 <- agg9020[order(agg9020$COMID, agg9020$doy),]
+
+# Combine localized model, future model for 1990-2020 period, and empirical data
+agg9020 <- dplyr::left_join(retro_agg, agg9020, by = c("COMID", "doy"))
+agg9020 <- as.data.frame(dplyr::left_join(agg9020, emp_agg, by = c("COMID", "doy")))
+data.table::fwrite(agg9020, "data/agg9020.csv")
+
+# examine for a day in Aug
+foo <- agg9020[agg9020$doy %in% 220 & !is.na(agg9020$mean_AvgDailyTemp),]
+summary(lm(foo$mean_STvalue ~ foo$mean_prd.stream_temp))
+summary(lm(foo$mean_AvgDailyTemp ~ foo$mean_prd.stream_temp))
+plot(foo$mean_prd.stream_temp, foo$mean_STvalue, ylim = c(5,25), xlim = c(5,25))
+points(foo$mean_prd.stream_temp, foo$mean_AvgDailyTemp, col = 2)
+abline(a = 4.76870, b = 0.69735)
+abline(a = -0.46426, b = 1.06832, col = 2)
+abline(a = 0, b = 1, lty = 3, col = "gray50") #1:1 line
+summary(foo)
+  # Conclusion: the future stream temp model definitely needs to be adjusted (black dots/line on graph) 
+  # The localized model matches the empirical data better in terms of the slope (red dots/line on graph); see evaluation above ~ line 71
+
+# Create adjustment
+agg9020$pAdj <- (agg9020$mean_prd.stream_temp + 1) / (agg9020$mean_STvalue + 1)
+agg9020$pAdj05 <- (agg9020$q05_prd.stream_temp + 1) / (agg9020$q05_STvalue + 1)
+agg9020$pAdj95 <- (agg9020$q95_prd.stream_temp + 1) / (agg9020$q95_STvalue + 1)
+data.table::fwrite(agg9020, "data/agg9020.csv")
+
+hist(agg9020$pAdj, breaks = 40)
+hist(agg9020$pAdj05, breaks = 40, col = rgb(0,0,1,0.5), add = T)
+hist(agg9020$pAdj95, breaks = 40, col = rgb(1,0,0,0.5), add = T)
+
+
+# Adjust future predictions based on 1990-2020 period
+fut_pds <- fut_long[lubridate::year(fut_long$Date) > 2020,]
+fut_pds$doy <- lubridate::yday(fut_pds$Date)
+fut_pds <- dplyr::left_join(fut_pds, agg9020[, c("COMID", "doy", "pAdj", "pAdj05", "pAdj95")], by = c("COMID", "doy"))
+
+fut_pds$STadj <- fut_pds$STvalue * fut_pds$pAdj
+fut_pds$STadj05 <- fut_pds$STvalue * fut_pds$pAdj05
+fut_pds$STadj95 <- fut_pds$STvalue * fut_pds$pAdj95
+data.table::fwrite(fut_pds, "data/future_adjusted.csv")
+
+hist(fut_pds$STadj, breaks = 40)
+hist(fut_pds$STadj05, breaks = 40, col = rgb(0,0,1,0.5), add = T)
+hist(fut_pds$STadj95, breaks = 40, col = rgb(1,0,0,0.5), add = T)
+
+
+# Aggregate by day of year and reach for 2021-2049 period
+period_data <- fut_pds[lubridate::year(fut_pds$Date) > 2020 & lubridate::year(fut_pds$Date) < 2050, c("Date", "COMID", "STadj", "STadj05", "STadj95")]
+agg2149 <- cbind(fncAggBy(dat = period_data[,c("Date", "COMID", "STadj05")], stat = "q", p = 0.05), 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj")], stat = "mean")[,3], 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj95")], stat = "q", p = 0.95)[,3])
+agg2149$doy <- as.numeric(agg2149$doy); agg2149 <- agg2149[order(agg2149$COMID, agg2149$doy),]
+data.table::fwrite(agg2149, "data/agg2149.csv")
+
+# Aggregate by day of year and reach for 2050-2074 period
+period_data <- fut_pds[lubridate::year(fut_pds$Date) > 2049 & lubridate::year(fut_pds$Date) < 2075, c("Date", "COMID", "STadj", "STadj05", "STadj95")]
+agg5074 <- cbind(fncAggBy(dat = period_data[,c("Date", "COMID", "STadj05")], stat = "q", p = 0.05), 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj")], stat = "mean")[,3], 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj95")], stat = "q", p = 0.95)[,3])
+agg5074$doy <- as.numeric(agg5074$doy); agg5074 <- agg5074[order(agg5074$COMID, agg5074$doy),]
+data.table::fwrite(agg5074, "data/agg5074.csv")
+
+# Aggregate by day of year and reach for 2075-2099 period
+period_data <- fut_pds[lubridate::year(fut_pds$Date) > 2075, c("Date", "COMID", "STadj", "STadj05", "STadj95")]
+agg7599 <- cbind(fncAggBy(dat = period_data[,c("Date", "COMID", "STadj05")], stat = "q", p = 0.05), 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj")], stat = "mean")[,3], 
+                 fncAggBy(dat = period_data[,c("Date", "COMID", "STadj95")], stat = "q", p = 0.95)[,3])
+agg7599$doy <- as.numeric(agg7599$doy); agg7599 <- agg7599[order(agg7599$COMID, agg7599$doy),]
+data.table::fwrite(agg7599, "data/agg7599.csv")
+
+
+# Figure 6A ----
+agg9020 <- as.data.frame(data.table::fread("data/agg9020.csv"))
+agg2149 <- as.data.frame(data.table::fread("data/agg2149.csv"))
+agg5074 <- as.data.frame(data.table::fread("data/agg5074.csv"))
+agg7599 <- as.data.frame(data.table::fread("data/agg7599.csv"))
+
+for(i in 1:5){
+  if(i == 1) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "white"]; nm <- "White River"} 
+  if(i == 2) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "chiwawa"]; nm <- "Chiwawa River"} 
+  if(i == 3) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "nason"]; nm <- "Nason Creek"}
+  if(i == 4) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenlower"]; nm <- "Wenatchee River"}
+  if(i == 5) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenmigr"]; nm <- "Migration Corridor"}
+
+  # Aggregate over the cids selected
+  dd <- agg9020[agg9020$COMID %in% cids,]
+  dd1 <- dd %>% group_by(doy) %>% reframe(mean(mean_prd.stream_temp, na.rm = T))
+  dd2 <- dd %>% group_by(doy) %>% reframe(mean(q05_prd.stream_temp, na.rm = T))
+  dd3 <- dd %>% group_by(doy) %>% reframe(mean(q95_prd.stream_temp, na.rm = T))
+  dd <- cbind(dd1, dd2[,2], dd3[,2]); colnames(dd) <- c("doy", "mean_prd.stream_temp", "q05_prd.stream_temp", "q95_prd.stream_temp")
+  q05_9020 <- predict(loess(dd$q05_prd.stream_temp ~ dd$doy, span = 0.5))
+  q95_9020 <- predict(loess(dd$q95_prd.stream_temp ~ dd$doy, span = 0.5))
+  
+  dd <- agg2149[agg2149$COMID %in% cids,]
+  dd1 <- dd %>% group_by(doy) %>% reframe(mean(mean_STadj, na.rm = T))
+  dd2 <- dd %>% group_by(doy) %>% reframe(mean(q05_STadj05, na.rm = T))
+  dd3 <- dd %>% group_by(doy) %>% reframe(mean(q95_STadj95, na.rm = T))
+  dd <- cbind(dd1, dd2[,2], dd3[,2]); colnames(dd) <- c("doy", "mean_STadj", "q05_STadj05", "q95_STadj95")
+  q05_2149 <- predict(loess(dd$q05_STadj05 ~ dd$doy, span = 0.5))
+  q95_2149 <- predict(loess(dd$q95_STadj95 ~ dd$doy, span = 0.5))
+  
+  dd <- agg5074[agg5074$COMID %in% cids,]
+  dd1 <- dd %>% group_by(doy) %>% reframe(mean(mean_STadj, na.rm = T))
+  dd2 <- dd %>% group_by(doy) %>% reframe(mean(q05_STadj05, na.rm = T))
+  dd3 <- dd %>% group_by(doy) %>% reframe(mean(q95_STadj95, na.rm = T))
+  dd <- cbind(dd1, dd2[,2], dd3[,2]); colnames(dd) <- c("doy", "mean_STadj", "q05_STadj05", "q95_STadj95")
+  q05_5074 <- predict(loess(dd$q05_STadj05 ~ dd$doy, span = 0.5))
+  q95_5074 <- predict(loess(dd$q95_STadj95 ~ dd$doy, span = 0.5))
+  
+  dd <- agg7599[agg7599$COMID %in% cids,]
+  dd1 <- dd %>% group_by(doy) %>% reframe(mean(mean_STadj, na.rm = T))
+  dd2 <- dd %>% group_by(doy) %>% reframe(mean(q05_STadj05, na.rm = T))
+  dd3 <- dd %>% group_by(doy) %>% reframe(mean(q95_STadj95, na.rm = T))
+  dd <- cbind(dd1, dd2[,2], dd3[,2]); colnames(dd) <- c("doy", "mean_STadj", "q05_STadj05", "q95_STadj95")
+  q05_7599 <- predict(loess(dd$q05_STadj05 ~ dd$doy, span = 0.5))
+  q95_7599 <- predict(loess(dd$q95_STadj95 ~ dd$doy, span = 0.5))
+  
+  emp_agg1 <- as.data.frame(emp_agg[emp_agg$COMID %in% cids,])
+  dd1 <- emp_agg1 %>% group_by(doy) %>% reframe(mean(mean_AvgDailyTemp, na.rm = T))
+  dd2 <- emp_agg1 %>% group_by(doy) %>% reframe(mean(q05_AvgDailyTemp, na.rm = T))
+  dd3 <- emp_agg1 %>% group_by(doy) %>% reframe(mean(q95_AvgDailyTemp, na.rm = T))
+  emp_agg1 <- cbind(dd1, dd2[,2], dd3[,2]); colnames(emp_agg1) <- c("doy", "mean_AvgDailyTemp", "q05_AvgDailyTemp", "q95_AvgDailyTemp")
+  q05_emp <- predict(loess(emp_agg1$q05_AvgDailyTemp ~ emp_agg1$doy, span = 0.5))
+  q95_emp <- predict(loess(emp_agg1$q95_AvgDailyTemp ~ emp_agg1$doy, span = 0.5))
+
+  colrs <- c("#FF990080", "#994F0080", "#0C7BDC80", "#0C9BAA80")
+  ylms <- c(0,22)
+  
+  png(paste0("plots/annual_", nm, ".png"), width = 6, height = 5, units = "in", res = 300)
+  plot(1:366, p95_emp, cex = .3, type = "n",
+       main = nm, las = 1, ylab = "Stream temperature (C)", xlab = "Date", ylim = ylms)
+  polygon(c(1:366, rev(1:366)), c(q05_7599, rev(q95_7599)), col = colrs[1], border = NA)
+  polygon(c(1:366, rev(1:366)), c(q05_5074, rev(q95_5074)), col = colrs[2], border = NA)
+  polygon(c(1:366, rev(1:366)), c(q05_2149, rev(q95_2149)), col = colrs[3], border = NA)
+  polygon(c(1:366, rev(1:366)), c(q05_9020, rev(q95_9020)), col = colrs[4], border = NA)
+  #empirical:
+  lines(1:length(q05_emp), q05_emp)
+  lines(1:length(q95_emp), q95_emp)
+
+  abline(h = seq(0,20,5), lty = 3, col = "gray40")
+  abline(v = pretty(dd$doy), lty = 3, col = "gray40")
+  if(i == 1) {legend("topleft", legend = c("far future", "near future", "current", "past", "empirical"), col = c(colrs, "black"), lwd = 2)}
+  dev.off()  
+}
+
+# mean August metric by year
+mnAugByYear <- fncAggBy(dat = fut_pds[, c("Date", "COMID", "STadj")], filter_month = 8, reach = COMID, timestep = year, stat = "mean")
+mnAugByYear95 <- fncAggBy(dat = fut_pds[, c("Date", "COMID", "STadj95")], filter_month = 8, reach = COMID, timestep = year, stat = "q", p = 0.95)
+
+  colrs <- c("gray", "#0C9BAA80", "#0C7BDC80", "#FF990080", "#994F0080")
+  
+  dat2plot <- as.data.frame(mnAugByYear); var <- "mean_STadj"
+  #dat2plot <- as.data.frame(mnAugByYear95); var <- "q95_STadj95"
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "white"]
+  plot(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[,var][dat2plot$COMID %in% cids], 
+       las = 1, ylab = "Stream temperature (C)", xlab = "Date", ylim = c(10,25), type = 'n')
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "chiwawa"]
+  points(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[, var][dat2plot$COMID %in% cids], pch = 19, col = colrs[2])
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "nason"]
+  points(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[, var][dat2plot$COMID %in% cids], pch = 19, col = colrs[3])
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenmigr"]
+  points(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[, var][dat2plot$COMID %in% cids], pch = 19, col = colrs[4])
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenlower"]
+  points(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[, var][dat2plot$COMID %in% cids], pch = 19, col = colrs[5])
+  cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "white"]
+  points(dat2plot$year[dat2plot$COMID %in% cids], dat2plot[, var][dat2plot$COMID %in% cids], pch = 19, col = colrs[1])
+  abline(h = seq(10,26,2), lty = 3, col = "gray")
+  abline(v = seq(2020, 2100,10), lty = 3, col = "gray")
+  abline(h = c(17, 20))
+  
+# # Get migration corridor
+# library(nhdplusTools)
+# streams <- sf::st_read("data/shp/Wenatchee_NHDv2_vat.shp")|>
+#   sf::st_transform('+proj=longlat +datum=WGS84') |>
+#   sf::st_zm()
+# ds.comids <- get_DM(streams, 23080722)
+# #us.comids <- get_UT(streams, start_comid)
+# plot(sf::st_geometry(sf::st_zm(streams)), col = "gray")
+# plot(sf::st_geometry(sf::st_zm(dplyr::filter(streams, COMID %in% ds.comids))), add = T,lwd = 2.5, col = "darkorange") #downstream reaches 
+
---
+++ code/3.1b_CalcPSM.R
@@ -0,0 +1,174 @@
+# Prespawn mortality calculation from:
+
+################################################################################
+##  Bowerman et al. 2021 Final top ranked models (by AIC) for Upper Columbia 
+##  spring Chinook salmon PSM mortality 
+#################################################################################
+
+#TOPUpCSP <- glmer(PSM ~  cTemp + Origin +  cJ.Day +
+#                    (1|Stream.section) + (1|Year),  
+#                  data=UpCSP4, family=binomial)
+
+
+library(ggplot2)
+library(rlang)
+library(dplyr)
+
+huc <- 17020011
+spawn_reaches <- read.csv("data/Wenatchee_spawn_reaches.csv")
+cids <- spawn_reaches$COMID
+
+fut_pds <- data.table::fread("data/future_adjusted.csv")
+st <- fut_pds[fut_pds$COMID %in% cids & lubridate::month(fut_pds$Date) %in% 8,]
+
+
+# Add origin, where hatchery = 1, natural = 0
+set.seed(123)
+st1 <- st
+st1$Origin <- 0
+st2 <- st
+st2$Origin <- 1
+st <- rbind(st1, st2); rm(st1, st2)
+
+# Add length
+cJ.Length <- round(seq(600, 1000, length = 20), 0)
+st$Length <- rnorm(n = nrow(st), mean = mean(cJ.Length), sd = sd(cJ.Length))
+# paper: length mean cm 81.36, sd 9.69
+hist(st$Length); range(st$Length)
+par(mfrow = c(2,1)); hist(st$Length[st$Origin == 0]); hist(st$Length[st$Origin == 1]); par(mfrow = c(1,1))
+
+# Center the data
+st$Temp <- (st[,"STadj"] - 16.47984)/2.556276
+st$Lng <- (st$Length - 813.6361)/96.89191
+
+# Add day (already centered)
+cJ.Day <- round(seq(-12,12, length = 20),0)
+st$Day <- rnorm(n = nrow(st), mean = mean(cJ.Day), sd = sd(cJ.Day))
+#st$Day = sample(cJ.Day, nrow(st), T)
+
+idx <- which(is.na(st[,"STadj"]))
+if(length(idx) > 0) {st <- st[-idx,]}
+
+# Model formula for the Upper Columbia model with the lowest AIC
+Mod.Mat <- model.matrix(~Temp + Origin + Day, data = st)
+
+#read in fixed effects
+fixef.all <-  read.csv(file="data/UCpublishedCOEF.csv", row.names=1)
+fixef <- fixef.all[,1]
+# Spring Chinook parameter estimates from model-averaged estimates in Fig 4:
+# Day -1, Length 0.5, Origin 1, Temp 1, Work 2; only Day, Origin and Temp significant
+
+NewData <- data.frame(st, eta = Mod.Mat %*% fixef)
+NewData <- with(NewData, data.frame(NewData, PSMPred = exp(eta) / (1+exp(eta)) ))   ## Predict PSM
+
+## read in covariance matrix
+vcov.mat <- as.matrix(read.csv(file="data/UCPublished_VCOV.csv", row.names=1))
+
+# eta = X*beta + Z*b + eps
+
+#Get the standard errors
+VAR <- diag(Mod.Mat %*% vcov.mat %*% t(Mod.Mat))   ##these are variances (sqrt of diag elements)
+NewData <- data.frame(NewData, pse = sqrt(VAR))   ## pse is p std error
+
+## estimate across range of data high/low CIs
+NewData <- with(NewData,
+                data.frame(NewData,
+                           psmlo = exp(eta - 1.96 * pse) / (1 + exp(eta - 1.96 * pse)),
+                           psmhi = exp(eta + 1.96 * pse) / (1 + exp(eta + 1.96 * pse))))
+
+
+# Save data
+write.csv(NewData, paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
+
+
+
+# Figure 7 ---- 
+NewData <- data.table::fread(paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
+d2p <- NewData[NewData$Origin == 0, c("year", "COMID", "ST.metric", "PSMPred", "psmlo", "psmhi")]
+d2p <- d2p[order(d2p$year, d2p$COMID),]
+#d2p <- d2p[d2p$COMID %in% cids,]
+
+yrs <- sort(unique(d2p$year))
+result <- d2p %>% 
+  group_by(year) %>%
+  reframe(quantile(psmhi, c(0.05, 0.5, 0.95), na.rm = T), prob = c(0.05, 0.5, 0.95))
+colnames(result) <- c("year", "psmhi", "quantile")
+
+psm.min <- result[result$quantile == 0.05,-ncol(result)]
+psm.max <- result[result$quantile == 0.95,-ncol(result)]
+psm.med <- result[result$quantile == 0.5,-ncol(result)]
+psm.mn <- d2p %>% 
+  group_by(year) %>%
+  reframe("psmhi" = mean(psmhi, na.rm = T))
+
+f1 <- predict(loess(psm.max$psmhi ~ psm.max$year, span = 0.5))
+f2 <- predict(loess(psm.mn$psmhi ~ psm.mn$year, span = 0.5))
+f3 <- predict(loess(psm.min$psmhi ~ psm.min$year, span = 0.5))
+
+st.mn <- d2p %>% 
+  group_by(year) %>%
+  reframe("ST.metric" = mean(ST.metric, na.rm = T))
+psm <- cbind(st.mn, psm.mn[,2], psm.min[,2], psm.med[,2], psm.max[,2])
+colnames(psm) <- c("year", "ST", "Mean", "Min", "Med", "Max")
+
+plot1 <- psm |> ggplot() +
+  geom_point(aes(x = ST, y = Mean, color = year), size = 3, alpha = 0.5)+
+  geom_point(aes(x = ST, y = Max, color = year), size = 3, alpha = 0.5, shape = 17)+
+  geom_point(aes(x = ST, y = Min, color = year), size = 3, alpha = 0.5, shape = 17)+
+  geom_smooth(aes(x = ST, y = Mean), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = ST, y = Max), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = ST, y = Min), method = "loess", se = TRUE, color = "gray50") +
+  scale_color_gradient(low = "blue", high = "red")+
+  theme(axis.text = element_text(size = 12))+
+  theme(axis.title = element_text(size = 12))+
+  theme(axis.title.y = element_text(vjust = 3.5))+
+  theme(axis.title.x = element_text(vjust = -1))+
+  labs(x = "Mean August stream temperature (°C)")+
+  labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
+  scale_x_continuous(limits = c(10, 20))+
+  scale_y_continuous(limits = c(0, 1))
+
+ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM.png"), plot1, height = 4.25, width = 6)
+
+
+plot1 <- psm |> ggplot() +
+  geom_point(aes(x = year, y = Mean, color = ST), size = 2, alpha = 0.5)+
+  geom_point(aes(x = year, y = Max, color = ST), size = 2, alpha = 0.5, shape = 17)+
+  geom_point(aes(x = year, y = Min, color = ST), size = 2, alpha = 0.5, shape = 15)+
+  geom_smooth(aes(x = year, y = Mean), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = year, y = Max), method = "loess", se = TRUE, color = "gray50") +
+  geom_smooth(aes(x = year, y = Min), method = "loess", se = TRUE, color = "gray50") +
+  scale_color_gradient(low = "blue", high = "red")+
+  theme(axis.text = element_text(size = 12))+
+  theme(axis.title = element_text(size = 12))+
+  theme(axis.title.y = element_text(vjust = 3.5))+
+  theme(axis.title.x = element_text(vjust = -1))+
+  labs(x = "Year")+
+  labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
+  #scale_x_continuous(limits = c(10, 20))+
+  scale_y_continuous(limits = c(0, 1))
+
+ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM_2.png"), plot1, height = 4.25, width = 6)
+
+
+# by Origin
+ggplot(NewData, aes(x = ST.metric, y = PSMPred, color = rev(as.factor(Origin)), border = rev(as.factor(Origin)), alpha = 0.1), size = 0.1)+
+  geom_point()+
+  theme(legend.position = "none")
+
+# Alternative format Figure 7A ----
+png("plots/PSM_over_time.png", width = 6, height = 6, units = "in", res = 300)
+
+plot(psm.max$year, psm.max$psmhi, cex = .3, type = "n",
+     las = 1, ylab = "Predicted pre-spawn mortality for wild Chinook salmon", xlab = "Year", ylim = c(0,1))
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f3)), col = "#0C7BDC80", border = NA)
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f2)), col = "#0C7BDC80", border = NA)
+#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f1)), col = "#0C7BDC80", border = NA)
+lines(psm.max$year, f3, lwd = 2, col = "#0965B5")
+lines(psm.max$year, f2, lwd = 2, col = "#0C7BDC")
+lines(psm.max$year, f1, lwd = 2, col = "#A9C9E8")
+points(psm.max$year, psm.min$psmhi, pch = 25, cex = 0.5, bg = "#0965B5", col = "#0965B5")
+points(psm.max$year, psm.mn$psmhi, pch = 21, , cex = 0.5, bg = "#0C7BDC", col = "#0C7BDC")
+points(psm.max$year, psm.max$psmhi, pch = 24, cex = 0.5, bg = "#A9C9E8", col = "#0C7BDC")
+
+dev.off()
