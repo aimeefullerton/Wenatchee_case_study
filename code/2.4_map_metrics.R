2166e51fd30c41e850076b26bfd0325b86ac0346
Aimee Fullerton <aimee.fullerton@noaa.gov>
2026-01-05 11:54:30 -0800

    major update

--- code/2.4_map_metrics.R
+++ code/2.4_map_metrics.R
@@ -20,11 +20,17 @@ wenbndry <- read_sf("data/shp/Wenatchee_boundary.shp")
 wenstreams <- read_sf("data/shp/Wenatchee_NHDv2_SOgt2.shp")
 streams <- dplyr::left_join(wenstreams, spawn_reaches, join_by(COMID))
 
-DADM7 <- data.table::fread(paste0("data/7DADM_", huc, ".csv"))[,-1]
-AugMn <- data.table::fread(paste0("data/AugMn_", huc, ".csv"))[,-1]
+
+mnAugByYear <- as.data.frame(data.table::fread("data/mnAugByYear.csv"))
+mnAugByYear$bin <- floor(mnAugByYear$year / 10) * 10
+mnAugByYear$bin_label = factor(mnAugByYear$bin)
 
 pal <- viridisLite::plasma(7)
-fncPlotMap <- function(temp_column = prd.stream_temp, legpos = "none", legnm = "7DADM", lims = c(5,34)){
+fncPlotMap <- function(temp_column = mean_STadj, yy, legpos = "none", legnm = "", lims = c(4,25)){
+  
+  sf_data <- mnAugByYear[mnAugByYear$bin %in% yy,] %>%
+    dplyr::left_join(streams, ., join_by(COMID)) 
+  
   plot_prd <- ggplot()+
     geom_sf(data = wenbndry, fill = "grey30", color = "grey30", size = 0.5) + 
     geom_sf(data = sf_data, aes(color = {{temp_column}}, linewidth = StreamOrde))+
@@ -36,96 +42,25 @@ fncPlotMap <- function(temp_column = prd.stream_temp, legpos = "none", legnm = "
     guides(linewidth = "none") +
     theme_void()+
     theme(legend.position = legpos)
+    #labs(title = yy)
   return(plot_prd)
 }
 
-# 7DADM ----
-yy <- 2000
-stmet1 <- DADM7[DADM7$year == yy,]
-sf_data <- stmet1 %>%
-  dplyr::left_join(streams, ., join_by(COMID)) %>%
-  filter(COMID %in% c)
-
-
-plot_prd.mx <- fncPlotMap(prd.stream_temp.mx, lims = c(0,28))
-plot_c.mx <- fncPlotMap(st.c.mnmx, lims = c(0,28))
-plot_nf.mx <- fncPlotMap(st.nf.mnmx, lims = c(0,28))
-plot_ff.mx <- fncPlotMap(st.ff.mnmx, legpos = "right", lims = c(0,28))
+p_2020 <- fncPlotMap(yy = 2020, legpos = "right")
+for(y in seq(2030, 2100, 10)){
+  p_yr <- fncPlotMap(yy = y)
+  assign(paste0("p_", y), p_yr)
+}
 
 
 combo_plot <- cowplot::plot_grid( # combine plots
- plot_prd.mx, plot_c.mx, plot_nf.mx, plot_ff.mx,
- labels = c("2000", "2025", "2050", "2075"),
+ #p_2020, p_2010, p_2020, p_2030, p_2040, p_2050, p_2060, p_2070, p_2080, p_2090,
+ p_2020, p_2040, p_2060, p_2080,
+ labels = c("2020s", "2040s", "2060s", "2080s"),
  label_colour = "black", label_size = 12)+
   theme(
     text = element_text(color = "black"),
     plot.background = element_rect(fill = "white", color =NA))
 
-ggsave(filename = paste0("plots/maps_7DADM_", huc, ".png"), combo_plot)
-
-
-# Map just the deltas:
-sf_data$delta.c <- sf_data$st.c.mnmx  - sf_data$prd.stream_temp.mx
-sf_data$delta.nf <- sf_data$st.nf.mnmx  - sf_data$prd.stream_temp.mx
-sf_data$delta.ff <- sf_data$st.ff.mnmx  - sf_data$prd.stream_temp.mx
-
-plot_c.mx <- fncPlotMap(delta.c, legnm = "delta 7DADM", lims = c(0,3))
-plot_nf.mx <- fncPlotMap(delta.nf, legnm = "delta 7DADM", lims = c(0,3))
-plot_ff.mx <- fncPlotMap(delta.ff, legpos = "right", legnm = "delta 7DADM", lims = c(0,3))
-
-combo_plot <- cowplot::plot_grid( # combine plots
-  plot_c.mx, plot_nf.mx, plot_ff.mx,
-  nrow = 1, rel_widths = c(0.68, 0.68, 1),
-  labels = c("current", "near future", "far future"),
-  label_colour = "black", label_size = 12)+
-  theme(
-    text = element_text(color = "black"),
-    plot.background = element_rect(fill = "white", color =NA))
-
-ggsave(filename = paste0("plots/maps_delta7DADM_", huc, ".png"), combo_plot)
-
-
-# AugMn ----
-yy <- 2000
-stmet1 <- AugMn[AugMn$year == yy,]
-sf_data <- stmet1 %>%
-  dplyr::left_join(streams, ., join_by(COMID)) %>%
-  filter(COMID %in% c)
-
-
-plot_prd <- fncPlotMap(prd.stream_temp, lims = c(0,25))
-plot_c <- fncPlotMap(cmn, lims = c(0,25))
-plot_nf <- fncPlotMap(nmn, lims = c(0,25))
-plot_ff <- fncPlotMap(fmn, legpos = "right", legnm = "AugMn", lims = c(0,25))
-
-combo_plot <- cowplot::plot_grid( # combine plots
-  plot_prd, plot_c, plot_nf, plot_ff,
-  labels = c("2000", "2025", "2050", "2075"),
-  label_colour = "black", label_size = 12)+
-  theme(
-    text = element_text(color = "black"),
-    plot.background = element_rect(fill = "white", color =NA))
-
-ggsave(filename = paste0("plots/maps_AugMn_", huc, ".png"), combo_plot)
-
-
-# Map just the deltas:
-sf_data$delta.c <- sf_data$cmn  - sf_data$prd.stream_temp
-sf_data$delta.nf <- sf_data$nmn  - sf_data$prd.stream_temp
-sf_data$delta.ff <- sf_data$fmn  - sf_data$prd.stream_temp
-
-plot_dc <- fncPlotMap(delta.c, legnm = "delta MnAug", lims = c(0,3))
-plot_dnf <- fncPlotMap(delta.nf, legnm = "delta MnAug", lims = c(0,3))
-plot_dff <- fncPlotMap(delta.ff, legpos = "right", legnm = "delta MnAug", lims = c(0,3))
-
-combo_plot <- cowplot::plot_grid( # combine plots
-  plot_dc, plot_dnf, plot_dff,
-  nrow = 1, rel_widths = c(0.68, 0.68, 1),
-  labels = c("current", "near future", "far future"),
-  label_colour = "black", label_size = 12)+
-  theme(
-    text = element_text(color = "black"),
-    plot.background = element_rect(fill = "white", color =NA))
-
-ggsave(filename = paste0("plots/maps_deltaAugMn_", huc, ".png"), combo_plot)
+ggsave(filename = paste0("plots/Wenatchee_mnAug_maps.png"), combo_plot)
 
