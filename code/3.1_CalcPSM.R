a0fe492118c381609eac6d9592d2fa8028a50efa
Aimee.Fullerton <1365855290113302@mil>
2026-01-06 14:22:32 -0800

    rename scripts and delete old ones

--- code/3.1_CalcPSM.R
+++ code/3.1_CalcPSM.R
@@ -11,19 +11,13 @@
 
 
 library(ggplot2)
-library(rlang)
 library(dplyr)
 
-huc <- 17020011
 spawn_reaches <- read.csv("data/Wenatchee_spawn_reaches.csv")
 cids <- spawn_reaches$COMID
-# NEED TO GET SPAWNING REACHES FOR ENTIAT AND METHOW TOO
 
-metric <- "AugMn" #or AugMn.lo, AugMn.hi
-
-stdat <- read.csv(paste0("data/prd.", metric, "_", huc, ".csv"))[,-1]
-st <- stdat[stdat$COMID %in% cids,]
-colnames(st)[3] <- "ST.metric"
+fut_pds <- data.table::fread("data/future_adjusted.csv")
+st <- fut_pds[fut_pds$COMID %in% cids & lubridate::month(fut_pds$Date) %in% 8,]
 
 
 # Add origin, where hatchery = 1, natural = 0
@@ -42,7 +36,7 @@ hist(st$Length); range(st$Length)
 par(mfrow = c(2,1)); hist(st$Length[st$Origin == 0]); hist(st$Length[st$Origin == 1]); par(mfrow = c(1,1))
 
 # Center the data
-st$Temp <- (st[,"ST.metric"] - 16.47984)/2.556276
+st$Temp <- (st[,"STadj"] - 16.47984)/2.556276
 st$Lng <- (st$Length - 813.6361)/96.89191
 
 # Add day (already centered)
@@ -50,7 +44,7 @@ cJ.Day <- round(seq(-12,12, length = 20),0)
 st$Day <- rnorm(n = nrow(st), mean = mean(cJ.Day), sd = sd(cJ.Day))
 #st$Day = sample(cJ.Day, nrow(st), T)
 
-idx <- which(is.na(st[,"ST.metric"]))
+idx <- which(is.na(st[,"STadj"]))
 if(length(idx) > 0) {st <- st[-idx,]}
 
 # Model formula for the Upper Columbia model with the lowest AIC
@@ -70,109 +64,142 @@ vcov.mat <- as.matrix(read.csv(file="data/UCPublished_VCOV.csv", row.names=1))
 
 # eta = X*beta + Z*b + eps
 
-#Get the standard errors
-VAR <- diag(Mod.Mat %*% vcov.mat %*% t(Mod.Mat))   ##these are variances (sqrt of diag elements)
-NewData <- data.frame(NewData, pse = sqrt(VAR))   ## pse is p std error
-
-## estimate across range of data high/low CIs
-NewData <- with(NewData,
-                data.frame(NewData,
-                           psmlo = exp(eta - 1.96 * pse) / (1 + exp(eta - 1.96 * pse)),
-                           psmhi = exp(eta + 1.96 * pse) / (1 + exp(eta + 1.96 * pse))))
+# #Get the standard errors (first need to subset data to avoid memory limit issues)
+# Not used in our application
+#
+# VAR <- diag(Mod.Mat %*% vcov.mat %*% t(Mod.Mat))   ##these are variances (sqrt of diag elements)
+# NewData <- data.frame(NewData, pse = sqrt(VAR))   ## pse is p std error
+# 
+# ## estimate across range of data high/low CIs
+# NewData <- with(NewData,
+#                 data.frame(NewData,
+#                            psmlo = exp(eta - 1.96 * pse) / (1 + exp(eta - 1.96 * pse)),
+#                            psmhi = exp(eta + 1.96 * pse) / (1 + exp(eta + 1.96 * pse))))
 
 
 # Save data
-write.csv(NewData, paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
+write.csv(NewData, paste0("data/Wenatchee_PSM.csv"))
 
 
 
 # Figure 7 ---- 
-NewData <- data.table::fread(paste0("data/UC_PSM_", huc, "_", metric, ".csv"))
-d2p <- NewData[NewData$Origin == 0, c("year", "COMID", "ST.metric", "PSMPred", "psmlo", "psmhi")]
-d2p <- d2p[order(d2p$year, d2p$COMID),]
-#d2p <- d2p[d2p$COMID %in% cids,]
+PSM <- data.table::fread(paste0("data/Wenatchee_PSM.csv"))
+PSM$year <- lubridate::year(PSM$Date)
+d2p <- PSM[PSM$Origin == 0, c("doy", "year", "COMID", "STadj", "PSMPred")]
+d2p <- d2p[order(d2p$COMID, d2p$year, d2p$doy),]
 
-yrs <- sort(unique(d2p$year))
-result <- d2p %>% 
-  group_by(year) %>%
-  reframe(quantile(psmhi, c(0.05, 0.5, 0.95), na.rm = T), prob = c(0.05, 0.5, 0.95))
-colnames(result) <- c("year", "psmhi", "quantile")
+# create 5-year bins and convert to factor so ggplot treats them as distinct groups
+d2p$bin <- floor(d2p$year / 10) * 10
+d2p$bin_label = factor(paste0(d2p$bin, "s"))
+d2p <- d2p[order(d2p$STadj),]
+d2p$bin2 <- floor(d2p$STadj / 5) * 5
+d2p$bin_label2 = factor(d2p$bin2)
 
-psm.min <- result[result$quantile == 0.05,-ncol(result)]
-psm.max <- result[result$quantile == 0.95,-ncol(result)]
-psm.med <- result[result$quantile == 0.5,-ncol(result)]
-psm.mn <- d2p %>% 
-  group_by(year) %>%
-  reframe("psmhi" = mean(psmhi, na.rm = T))
-
-f1 <- predict(loess(psm.max$psmhi ~ psm.max$year, span = 0.5))
-f2 <- predict(loess(psm.mn$psmhi ~ psm.mn$year, span = 0.5))
-f3 <- predict(loess(psm.min$psmhi ~ psm.min$year, span = 0.5))
-
-st.mn <- d2p %>% 
-  group_by(year) %>%
-  reframe("ST.metric" = mean(ST.metric, na.rm = T))
-psm <- cbind(st.mn, psm.mn[,2], psm.min[,2], psm.med[,2], psm.max[,2])
-colnames(psm) <- c("year", "ST", "Mean", "Min", "Med", "Max")
-
-plot1 <- psm |> ggplot() +
-  geom_point(aes(x = ST, y = Mean, color = year), size = 3, alpha = 0.5)+
-  geom_point(aes(x = ST, y = Max, color = year), size = 3, alpha = 0.5, shape = 17)+
-  geom_point(aes(x = ST, y = Min, color = year), size = 3, alpha = 0.5, shape = 17)+
-  geom_smooth(aes(x = ST, y = Mean), method = "loess", se = TRUE, color = "gray50") +
-  geom_smooth(aes(x = ST, y = Max), method = "loess", se = TRUE, color = "gray50") +
-  geom_smooth(aes(x = ST, y = Min), method = "loess", se = TRUE, color = "gray50") +
-  scale_color_gradient(low = "blue", high = "red")+
-  theme(axis.text = element_text(size = 12))+
-  theme(axis.title = element_text(size = 12))+
+# Create violin plot
+fncPlotPSM <- function(dat, i, var = bin_label, filenm = "by_year"){
+  
+if(i == 1) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "white"]; nm <- "White River"} 
+if(i == 2) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "chiwawa"]; nm <- "Chiwawa River"} 
+if(i == 3) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "nason"]; nm <- "Nason Creek"}
+if(i == 4) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenlower"]; nm <- "Wenatchee River"}
+if(i == 5) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenmigr"]; nm <- "Migration Corridor"}
+dat <- dat[dat$COMID %in% cids,]
+  
+p <- ggplot(dat, 
+  aes(x = {{var}}, y = PSMPred)) +
+  geom_violin(
+    fill = "purple4", color = "purple4",
+    trim = T, 
+    alpha = 0.4,
+    linewidth = 0.2) +
+  # Add a boxplot inside the violin to show median and quartiles
+  stat_boxplot(
+    geom = "errorbar", 
+    width = 0.1,            # Controls the width of the horizontal hatches
+    color = "gray10", 
+    linewidth = 0.5
+  ) +
+  geom_boxplot(width = 0.1, linewidth = 0.5, outlier.shape = NA,
+    fill = "purple4", 
+    color = "gray10",
+    alpha = 0.7) +
+  scale_y_continuous(limits = c(0, 1))+
+  labs(
+    title = nm,
+    x = "10-Year Period",
+    y = "Predicted prespawn mortality",
+  ) +
+  theme_minimal() +
+  theme(axis.text = element_text(size = 14))+
+  theme(axis.title = element_text(size = 14))+
   theme(axis.title.y = element_text(vjust = 3.5))+
   theme(axis.title.x = element_text(vjust = -1))+
-  labs(x = "Mean August stream temperature (°C)")+
-  labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
-  scale_x_continuous(limits = c(10, 20))+
-  scale_y_continuous(limits = c(0, 1))
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+  )+
+  theme(legend.position = "none") # Hide legend as x-axis labels are sufficient
 
-ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM.png"), plot1, height = 4.25, width = 6)
+filename <- paste0("plots/Wenatchee_vPSM_", filenm, "_", nm, ".png")
+ggsave(filename = filename, p, height = 3.5, width = 6)
+return(p)
+}
+fncPlotPSMbyST <- function(dat, i){
+  if(i == 1) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "white"]; nm <- "White River"} 
+  if(i == 2) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "chiwawa"]; nm <- "Chiwawa River"} 
+  if(i == 3) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "nason"]; nm <- "Nason Creek"}
+  if(i == 4) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenlower"]; nm <- "Wenatchee River"}
+  if(i == 5) {cids <- spawn_reaches$COMID[spawn_reaches$spawn_reach %in% "wenmigr"]; nm <- "Migration Corridor"}
+  dat <- dat[dat$COMID %in% cids,]
+  
+  p <- dat |> 
+    ggplot(aes(x = STadj, y = PSMPred)) +
+    # 1. Scatterplot Layer (Continuous)
+    geom_point(aes(color = year), size = 1, alpha = 0.05) +
+    
+    # 2. Violin Plot Layer (Discretized via grouping)
+    # Use 'group = five_year_bin' to force discrete violins on a continuous scale
+    geom_violin(
+      aes(x = STadj, group = bin_label2), 
+      fill = NA, 
+      color = "black",
+      trim = TRUE, 
+      linewidth = 0.5,
+      position = "identity" # Keeps them overlaid on the points
+    ) +
+    
+    # Styling and Scales
+    scale_color_gradient(low = "#4D9221", high = "#C51B7D") +
+    scale_x_continuous(limits = c(7, 28)) +
+    scale_y_continuous(limits = c(0, 1)) +
+    labs(
+      title = nm,
+      x = "Daily August stream temperature (°C)",
+      y = "Predicted pre-spawn mortality for wild Chinook salmon"
+    ) +
+    theme_minimal() +
+    theme(
+      axis.text = element_text(size = 14),
+      axis.title = element_text(size = 14),
+      axis.title.y = element_text(vjust = 3.5),
+      axis.title.x = element_text(vjust = -1),
+      # Reducing grid lines as requested previously
+      panel.grid.minor = element_blank()
+    )
+  filename <- paste0("plots/Wenatchee_PSM_by_ST_", nm, ".png")
+  ggsave(filename = filename, p, height = 3.5, width = 6)
+  return(p)
+}
+
+for(i in 1:5){
+  p_yr <- fncPlotPSM(d2p, i)
+  assign(paste0("p_yr", i), p_yr)
+  p_st <- fncPlotPSMbyST(d2p, i)
+  assign(paste0("p_st", i), p_st)
+}
 
 
-plot1 <- psm |> ggplot() +
-  geom_point(aes(x = year, y = Mean, color = ST), size = 2, alpha = 0.5)+
-  geom_point(aes(x = year, y = Max, color = ST), size = 2, alpha = 0.5, shape = 17)+
-  geom_point(aes(x = year, y = Min, color = ST), size = 2, alpha = 0.5, shape = 15)+
-  geom_smooth(aes(x = year, y = Mean), method = "loess", se = TRUE, color = "gray50") +
-  geom_smooth(aes(x = year, y = Max), method = "loess", se = TRUE, color = "gray50") +
-  geom_smooth(aes(x = year, y = Min), method = "loess", se = TRUE, color = "gray50") +
-  scale_color_gradient(low = "blue", high = "red")+
-  theme(axis.text = element_text(size = 12))+
-  theme(axis.title = element_text(size = 12))+
-  theme(axis.title.y = element_text(vjust = 3.5))+
-  theme(axis.title.x = element_text(vjust = -1))+
-  labs(x = "Year")+
-  labs(y = "Predicted pre-spawn mortality for wild Chinook salmon")+
-  #scale_x_continuous(limits = c(10, 20))+
-  scale_y_continuous(limits = c(0, 1))
-
-ggsave(filename = paste0("plots/", huc, "_", metric, "_PSM_2.png"), plot1, height = 4.25, width = 6)
-
-
-# by Origin
-ggplot(NewData, aes(x = ST.metric, y = PSMPred, color = rev(as.factor(Origin)), border = rev(as.factor(Origin)), alpha = 0.1), size = 0.1)+
-  geom_point()+
-  theme(legend.position = "none")
-
-# Alternative format Figure 7A ----
-png("plots/PSM_over_time.png", width = 6, height = 6, units = "in", res = 300)
-
-plot(psm.max$year, psm.max$psmhi, cex = .3, type = "n",
-     las = 1, ylab = "Predicted pre-spawn mortality for wild Chinook salmon", xlab = "Year", ylim = c(0,1))
-#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f3)), col = "#0C7BDC80", border = NA)
-#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f2)), col = "#0C7BDC80", border = NA)
-#polygon(c(psm.max$year, rev(psm.max$year)), c(rep(0,100), rev(f1)), col = "#0C7BDC80", border = NA)
-lines(psm.max$year, f3, lwd = 2, col = "#0965B5")
-lines(psm.max$year, f2, lwd = 2, col = "#0C7BDC")
-lines(psm.max$year, f1, lwd = 2, col = "#A9C9E8")
-points(psm.max$year, psm.min$psmhi, pch = 25, cex = 0.5, bg = "#0965B5", col = "#0965B5")
-points(psm.max$year, psm.mn$psmhi, pch = 21, , cex = 0.5, bg = "#0C7BDC", col = "#0C7BDC")
-points(psm.max$year, psm.max$psmhi, pch = 24, cex = 0.5, bg = "#A9C9E8", col = "#0C7BDC")
-
-dev.off()
+# # by Origin
+# ggplot(PSM, aes(x = STadj, y = PSMPred, color = rev(as.factor(Origin)), border = rev(as.factor(Origin)), alpha = 0.1), size = 0.1)+
+#   geom_point()+
+#   theme(legend.position = "none")
